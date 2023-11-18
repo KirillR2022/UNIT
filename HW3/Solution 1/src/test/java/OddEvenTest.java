b7d59e190ccfa4073adc5865804ef8a05b32cfd9
Kirill <kirill075@mail.ru>
2023-11-18 21:55:04 +0300

    Add HW4 and refactor HW3

--- HW3/Solution 1/src/test/java/OddEvenTest.java
+++ HW3/Solution 1/src/test/java/OddEvenTest.java
@@ -6,41 +6,31 @@ public class OddEvenTest {
 
     @Test
     public void testEvenNumber() {
-        YourClass yourObject = new YourClass(); // Замените YourClass на имя вашего класса
-
-        // Проверяем, что метод правильно определяет четное число
-        assertTrue(yourObject.evenOddNumber(4));
+        OddEven oddEvent = new OddEven();
+        assertTrue(oddEvent.evenOddNumber(4));
     }
 
     @Test
     public void testOddNumber() {
-        YourClass yourObject = new YourClass(); // Замените YourClass на имя вашего класса
-
-        // Проверяем, что метод правильно определяет нечетное число
-        assertFalse(yourObject.evenOddNumber(7));
+        OddEven oddEvent = new OddEven();
+        assertFalse(oddEvent.evenOddNumber(7));
     }
 
     @Test
     public void testZero() {
-        YourClass yourObject = new YourClass(); // Замените YourClass на имя вашего класса
-
-        // Проверяем, что метод правильно обрабатывает ноль
-        assertTrue(yourObject.evenOddNumber(0));
+        OddEven oddEvent = new OddEven();
+        assertTrue(oddEvent.evenOddNumber(0));
     }
 
     @Test
     public void testNegativeEvenNumber() {
-        YourClass yourObject = new YourClass(); // Замените YourClass на имя вашего класса
-
-        // Проверяем, что метод правильно определяет четное отрицательное число
-        assertTrue(yourObject.evenOddNumber(-2));
+        OddEven oddEvent = new OddEven();
+        assertTrue(oddEvent.evenOddNumber(-2));
     }
 
     @Test
     public void testNegativeOddNumber() {
-        YourClass yourObject = new YourClass(); // Замените YourClass на имя вашего класса
-
-        // Проверяем, что метод правильно определяет нечетное отрицательное число
-        assertFalse(yourObject.evenOddNumber(-3));
+        OddEven oddEvent = new OddEven();
+        assertFalse(oddEvent.evenOddNumber(-3));
     }
 }
